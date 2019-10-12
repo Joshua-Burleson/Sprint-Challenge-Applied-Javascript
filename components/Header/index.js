be091066e2fb99124bd44e10887d62bfdc6ed36a
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-10-11 20:24:20 -0400

    MVP completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,6 +9,8 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
+
+            // If I could edit the HTML I would import this function as a module script, instead I'm going to break DRY and copy-paste it.
 const wrapAndAdd = (wrapper, childElementsInOrder, insertionLocation) => {
     childElementsInOrder.forEach(element => wrapper.appendChild(element));
     return document.querySelector(`${insertionLocation}`).appendChild(wrapper);
